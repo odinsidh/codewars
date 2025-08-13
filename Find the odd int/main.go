7a14ac5abf2a6f4bae96b17750597519fbb323b3
odinsidh <odinsidh@gmail.com>
2025-08-13 06:19:07 +0300

    solve: Find the odd int

--- Find the odd int/main.go
+++ Find the odd int/main.go
@@ -5,11 +5,20 @@ import "fmt"
 var console = fmt.Println
 var consolef = fmt.Printf
 
-func FindOdd(seq []int) int {
-	return 0
+func FindOdd(input []int) (output int) {
+	var container map[int]int = make(map[int]int)
+	for _, value := range input {
+		container[value] += 1
+	}
+	for key, value := range container {
+		if value%2 != 0 {
+			output = key
+		}
+	}
+	return
 }
 
 func main() {
-	var d1 []int = []int{20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5}
+	var d1 []int = []int{10}
 	console(FindOdd(d1))
 }
