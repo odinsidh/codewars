1b6aed5af82a466ff49ead0c6596207c719da6bf
odinsidh <odinsidh@gmail.com>
2025-08-14 08:26:20 +0300

    solve: Numericals of a String

--- Numericals of a String/main.go
+++ Numericals of a String/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var console = fmt.Println
+var consolef = fmt.Printf
+
+func Numericals(input string) string {
+	var (
+		seen   map[rune]int = make(map[rune]int)
+		output []string
+	)
+	for _, value := range input {
+		seen[value] += 1
+		output = append(output, strconv.Itoa(seen[value]))
+	}
+	return strings.Join(output, "")
+}
+
+func main() {
+	var d1 string = "Hello, World!"
+	console(Numericals(d1))
+}
