bbdfa83326f6463cf37308f7a24b49d0ef26803f
odinsidh <odinsidh@gmail.com>
2025-08-16 07:53:52 +0300

    solve: Pairs of Bears

--- Pairs of Bears/main.go
+++ Pairs of Bears/main.go
@@ -1,3 +1,28 @@
+// https://www.codewars.com/kata/57d165ad95497ea150000020/train/go
 package main
 
-func main() {}
+import (
+	"fmt"
+)
+
+var console = fmt.Println
+var consolef = fmt.Printf
+
+func CheckPairs(needed int, input string) (string, bool) {
+	var output string
+	for i := 0; i < len(input)-1; i++ {
+		for _, value := range []string{"8B", "B8"} {
+			if input[i] == value[0] && input[i+1] == value[1] {
+				output += string(input[i : i+2])
+				i++
+				break
+			}
+		}
+	}
+	return output, len(output) >= needed
+}
+
+func main() {
+	var d1, d2 = 7, "8j8mBliB8gimjB8B8jlB"
+	console(CheckPairs(d1, d2))
+}
