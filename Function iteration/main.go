6fdbc3ece48589069bf8cea4227068e42fa9a77f
odinsidh <odinsidh@gmail.com>
2025-08-16 09:22:08 +0300

    solve : Function iteration

--- Function iteration/main.go
+++ Function iteration/main.go
@@ -1,3 +1,25 @@
+// https://www.codewars.com/kata/54b679eaac3d54e6ca0008c9/go
 package main
 
-func main() {}
+import "fmt"
+
+var console = fmt.Println
+var consolef = fmt.Printf
+
+func getDouble(input int) int {
+	return input * 2
+}
+
+func CreateIterator(inputFunction func(int) int, inputAmount int) func(int) int {
+	return func(inputValue int) int {
+		for i := 0; i < inputAmount; i++ {
+			inputValue = inputFunction(inputValue)
+		}
+		return inputValue
+	}
+}
+
+func main() {
+	var call = CreateIterator(getDouble, 3)
+	console(call(3))
+}
