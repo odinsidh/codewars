fe54613c32600577a7c00a2392a223ed47c3259f
odinsidh <odinsidh@gmail.com>
2025-08-19 14:33:27 +0300

    solve: Counting Duplicates

--- Counting Duplicates/main.go
+++ Counting Duplicates/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"strings"
+)
+
+var console = fmt.Println
+var consolef = fmt.Printf
+
+func duplicate_count(input string) (output int) {
+	var knowledge map[rune]int = make(map[rune]int)
+	input = strings.ToLower(input)
+	for _, value := range input {
+		knowledge[value]++
+	}
+	for _, value := range knowledge {
+		if value > 1 {
+			output++
+		}
+	}
+	return
+}
+
+func main() {
+	var d1 string = "abcdeaB11"
+	console(duplicate_count(d1))
+}
