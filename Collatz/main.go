77072150399bf9f54909ec8759da6eb3e0618cd0
odinsidh <odinsidh@gmail.com>
2025-08-20 09:26:03 +0300

    solve : Collatz

--- Collatz/main.go
+++ Collatz/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+var console = fmt.Println
+var consolef = fmt.Printf
+
+func Collatz(input int) (output string) {
+	switch {
+	case input == 1:
+		return strconv.Itoa(input)
+	case input%2 == 0:
+		output = strconv.Itoa(input) + "->"
+		input = input / 2
+	case input%2 != 0:
+		output = strconv.Itoa(input) + "->"
+		input = input*3 + 1
+	}
+	return output + Collatz(input)
+}
+
+func main() {
+	console(Collatz(3))
+}
