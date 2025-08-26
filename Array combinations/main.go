cef4b036019041b136d52e295930a0823e9ae656
odinsidh <odinsidh@gmail.com>
2025-08-26 18:51:33 +0300

    solve : Array combinations

--- Array combinations/main.go
+++ Array combinations/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import "fmt"
+
+var console = fmt.Println
+var consolef = fmt.Printf
+
+func Solve(input [][]int) int {
+	var output int = 1
+	for _, v0 := range input {
+		var data map[int]struct{} = map[int]struct{}{}
+		for _, v1 := range v0 {
+			data[v1] = struct{}{}
+		}
+		output = output * len(data)
+	}
+	return output
+}
+
+func main() {
+	var d1 [][]int = [][]int{{1, 2, 3}, {3, 4, 6, 6, 7}, {8, 9, 10, 12, 5, 6}}
+	console(Solve(d1))
+}
